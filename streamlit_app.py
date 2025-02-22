cdd62f0e153d79992af2c55c267378baeb9fa017
Charles Mathew <52280002+cmkuruvi@users.noreply.github.com>
2025-02-22 01:14:04 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
-st.title("👔 AI-Powered Body Measurement Predictor - MEN")
+st.title("ORGANO
+👔 AI-Powered Body Measurement Predictor - MEN")
 
 # Function to load the dataset
 @st.cache_data
