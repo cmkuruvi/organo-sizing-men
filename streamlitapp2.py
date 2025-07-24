7fdfe0e0f3a52be159a870219d6b3e5a146bc10b
Charles Mathew <52280002+cmkuruvi@users.noreply.github.com>
2025-07-24 15:39:07 -0700

    Update streamlitapp2.py

--- streamlitapp2.py
+++ streamlitapp2.py
@@ -13,7 +13,7 @@ if password != VALID_PASSWORD:
     st.stop()
 
 # ------------------ APP HEADER -----------------
-st.image("Fitsall Logo new 1.png", width=200)
+st.image("Fitsall Logo new 1.png", width=400)
 st.title("👖 AI-Powered Body Measurement Predictor - MEN")
 
 # ------------------ DATA LOADING & MODEL -------------------
